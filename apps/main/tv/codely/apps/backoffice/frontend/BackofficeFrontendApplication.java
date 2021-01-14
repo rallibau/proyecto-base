e73d96577eacd0bcd36bf8e90907b04fbbcfa473
rallibau <rallibau@gmail.com>
2021-01-14 16:20:36 +0100

    Test

--- apps/main/tv/codely/apps/backoffice/frontend/BackofficeFrontendApplication.java
+++ apps/main/tv/codely/apps/backoffice/frontend/BackofficeFrontendApplication.java
@@ -11,9 +11,20 @@ import java.util.HashMap;
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 @ComponentScan(
     includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Service.class),
-    value = {"tv.codely.shared", "tv.codely.backoffice", "tv.codely.mooc", "tv.codely.apps.backoffice.frontend"}
+    value = {
+        BackofficeFrontendApplication.TV_CODELY_SHARED,
+        BackofficeFrontendApplication.TV_CODELY_BACKOFFICE,
+        BackofficeFrontendApplication.TV_CODELY_MOOC,
+        BackofficeFrontendApplication.TV_CODELY_APPS_BACKOFFICE_FRONTEND
+    }
 )
 public class BackofficeFrontendApplication {
+
+    public static final String TV_CODELY_SHARED = "tv.codely.shared";
+    public static final String TV_CODELY_BACKOFFICE = "tv.codely.backoffice";
+    public static final String TV_CODELY_MOOC = "tv.codely.mooc";
+    public static final String TV_CODELY_APPS_BACKOFFICE_FRONTEND = "tv.codely.apps.backoffice.frontend";
+
     public static HashMap<String, Class<?>> commands() {
         return new HashMap<String, Class<?>>() {{
         }};
